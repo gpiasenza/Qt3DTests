f24dc107cd894a5052df72f43ee4e5c8f3953d77
Guido Piasenza <gpiasenza@soft-in.com>
2020-06-05 10:50:06 +0200

    Added new class EntityWrapper to simplify our life

--- EntityWrapper.cpp
+++ EntityWrapper.cpp
@@ -1,6 +1,41 @@
 #include "EntityWrapper.h"
 
-EntityWrapper::EntityWrapper()
-{
+#include <Qt3DCore/QComponent>
+#include <Qt3DCore/QEntity>
+#include <Qt3DCore/QTransform>
+#include <Qt3DExtras/QPhongMaterial>
 
+EntityWrapper::EntityWrapper(Qt3DCore::QEntity *entity)
+{
+    this->entity = entity;
+
+    for(auto comp: entity->components())
+    {
+        if(!transform)
+            transform = qobject_cast<Qt3DCore::QTransform *>(comp);
+
+        if(!mat)
+            mat = qobject_cast<Qt3DExtras::QPhongMaterial *>(comp);
+    }
+
+    for(auto childNode: entity->childNodes())
+    {
+        auto childEntity = qobject_cast<Qt3DCore::QEntity *>(childNode);
+        if (childEntity)
+            children.push_back(new EntityWrapper(childEntity));
+    }
+}
+
+void EntityWrapper::Move(double x, double y, double z)
+{
+    if(transform)
+        transform->setTranslation(QVector3D(x, y, z));
+}
+
+void EntityWrapper::setSpecular(const QColor &col)
+{
+    if(mat)
+        mat->setSpecular(col);
+    for(auto child: children)
+        child->setSpecular(col);
 }
--- EntityWrapper.h
+++ EntityWrapper.h
@@ -2,10 +2,34 @@
 #define ENTITYWRAPPER_H
 
 
+namespace Qt3DCore
+{
+    class QEntity;
+    class QTransform;
+}
+
+namespace Qt3DExtras
+{
+    class QPhongMaterial;
+}
+
+#include <QMatrix4x4>
+#include <QList>
+#include <QMap>
+
 class EntityWrapper
 {
 public:
-    EntityWrapper();
+    EntityWrapper(Qt3DCore::QEntity *entity);
+
+    void Move(double x, double y, double z);
+    void setSpecular(const QColor &col);
+
+protected:
+    Qt3DCore::QEntity *entity = nullptr;
+    Qt3DCore::QTransform *transform = nullptr;
+    Qt3DExtras::QPhongMaterial *mat = nullptr;
+    QList<EntityWrapper *> children;
 };
 
 #endif // ENTITYWRAPPER_H
