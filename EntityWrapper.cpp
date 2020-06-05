d27b2b1c0bc3939257b0e98832d2a9051d775d0f
Guido Piasenza <gpiasenza@soft-in.com>
2020-06-05 10:49:28 +0200

    Added a second material to TorusMesh, assigning half of the torus to it

---
+++ EntityWrapper.cpp
@@ -0,0 +1,6 @@
+#include "EntityWrapper.h"
+
+EntityWrapper::EntityWrapper()
+{
+
+}
---
+++ EntityWrapper.h
@@ -0,0 +1,11 @@
+#ifndef ENTITYWRAPPER_H
+#define ENTITYWRAPPER_H
+
+
+class EntityWrapper
+{
+public:
+    EntityWrapper();
+};
+
+#endif // ENTITYWRAPPER_H
